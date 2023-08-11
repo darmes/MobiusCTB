377dfc0221d4f76e20321a3b67e80a85e426f461
darmes <darmes@users.noreply.github.com>
2023-08-11 16:11:47 -0400

    Add ability to get top/bottom elements/states

--- Source/Beastiary/Game_Enemy.rb
+++ Source/Beastiary/Game_Enemy.rb
@@ -19,11 +19,35 @@ class Game_Enemy < Game_Battler
   def element_ranks
     return $data_enemies[@enemy_id].element_ranks
   end
+  #--------------------------------------------------------------------------
+  # * Get Elements that the enemy is strong versus
+  #--------------------------------------------------------------------------
+  def strong_elements
+    return $data_enemies[@enemy_id].strong_elements
+  end
+  #--------------------------------------------------------------------------
+  # * Get Elements that the enemy is strong versus
+  #--------------------------------------------------------------------------
+  def weak_elements
+    return $data_enemies[@enemy_id].weak_elements
+  end
   #-------------------------------------------------------------------------- 
   # * Get State Efficiency - Human readable string
   #     state_id : State ID
   #--------------------------------------------------------------------------
   def state_efficiency(state_id)
     $data_enemies[@enemy_id].state_efficiency(state_id)
+  end
+  #--------------------------------------------------------------------------
+  # * Get States that the enemy is strong versus
+  #--------------------------------------------------------------------------
+  def strong_states
+    return $data_enemies[@enemy_id].strong_states
+  end
+  #--------------------------------------------------------------------------
+  # * Get States that the enemy is strong versus
+  #--------------------------------------------------------------------------
+  def weak_states
+    return $data_enemies[@enemy_id].weak_states
   end
 end
--- Source/RGSS/RPG_Enemy.rb
+++ Source/RGSS/RPG_Enemy.rb
@@ -6,6 +6,44 @@ module RPG
   class Enemy
     alias base_name name
     #--------------------------------------------------------------------------
+    # * Get IDs bundled with their Ranks
+    #--------------------------------------------------------------------------
+    def id_with_rank(rank_array)
+      size = rank_array.xsize - 1
+      result = []
+      for id in 1..size
+        rank = rank_array[id]
+        if yield(rank)
+          result.push([id, rank])
+        end
+      end
+      return result
+    end
+    #--------------------------------------------------------------------------
+    # * Get Strong Elements
+    #--------------------------------------------------------------------------
+    def strong_elements
+      strong_element_array = id_with_rank(element_ranks) { |rank| rank > 3 }
+      strong_element_array.sort! do |a,b|
+        b[1] <=> a[1] # Sort greater rank (6) to front of array (lower index)
+      end
+      return strong_element_array.map do |tuple|
+        tuple[0] # return just the ID
+      end
+    end
+    #--------------------------------------------------------------------------
+    # * Get Weak Elements
+    #--------------------------------------------------------------------------
+    def weak_elements
+      weak_element_array = id_with_rank(element_ranks) { |rank| rank < 3 }
+      weak_element_array.sort! do |a,b|
+        a[1] <=> b[1] # Sort lower rank (1) to front of array (lower index)
+      end
+      return weak_element_array.map do |tuple|
+        tuple[0] # return just the ID
+      end
+    end
+    #--------------------------------------------------------------------------
     # * Get Element Efficiency - Human readable string
     #     element_id : Element ID
     #--------------------------------------------------------------------------
@@ -30,6 +68,46 @@ module RPG
         return Mobius::Beastiary::ELEMENT_WORD_0
       when 6 # Absorb = -100%
         return Mobius::Beastiary::ELEMENT_WORD_M100
+      end
+    end
+    #--------------------------------------------------------------------------
+    # * Get Strong States
+    #--------------------------------------------------------------------------
+    def strong_states
+      strong_state_array = id_with_rank(state_ranks) { |rank| rank > 3 }
+      strong_state_array.sort do |a,b|
+        aID = a[0]
+        aRank = a[1]
+        aRating = $data_states[aID].rating
+        bID = b[0]
+        bRank = b[1]
+        bRating = $data_states[bID].rating
+        # Sort greater rank (6) to front of array (lower index)
+        # If ranks are equal, sort higher rating to front
+        [bRank, bRating] <=> [aRank, aRating]
+      end
+      return strong_state_array.map do |tuple|
+        tuple[0] # return just the ID
+      end
+    end
+    #--------------------------------------------------------------------------
+    # * Get Weak States
+    #--------------------------------------------------------------------------
+    def weak_states
+      weak_state_array = id_with_rank(state_ranks) { |rank| rank < 3 }
+      weak_state_array.sort! do |a,b|
+        aID = a[0]
+        aRank = a[1]
+        aRating = $data_states[aID].rating
+        bID = b[0]
+        bRank = b[1]
+        bRating = $data_states[bID].rating
+        # Sort lower rank (1) to front of array (lower index)
+        # If ranks are equal, sort higher rating to front
+        [aRank, bRating] <=> [bRank, aRating]
+      end
+      return weak_state_array.map do |tuple|
+        tuple[0] # return just the ID
       end
     end
     #-------------------------------------------------------------------------- 
